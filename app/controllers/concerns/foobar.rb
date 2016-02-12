52811b53d917ec063eb51a6dd71b4f5de1809296
Erica <ericalmeng@gmail.com>
2016-02-11 19:11:48 -0800

    Finish

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,3 +1,11 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
+  def initialize(name)
+    @name = name
+  end
+
+  def bar(cat_symbol, hash)
+    return cat_symbol.to_s + @name + hash[:sat].to_s
+  end
+  
 end
---
+++ app/controllers/concerns/person.rb
@@ -0,0 +1,21 @@
+class Person
+    
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @nickname = name[0..3]
+    end
+
+    def introduce
+        return "Your name is " + @name+" and your are "+@age+" years old."
+    end
+
+    def birth_year
+        return 2015-@age.to_i
+    end
+
+    def nickname
+        return @nickname
+    end
+
+end
